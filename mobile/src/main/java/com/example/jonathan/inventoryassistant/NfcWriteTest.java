8808b62f00c93edeb682eee7aa6235e9d4f56177
portdevil <portdevil@gmail.com>
2015-07-31 15:54:47 -0700

    Clean up uneeded code segments in ReadTest and WriteTest

--- mobile/src/main/java/com/example/jonathan/inventoryassistant/NfcWriteTest.java
+++ mobile/src/main/java/com/example/jonathan/inventoryassistant/NfcWriteTest.java
@@ -2,7 +2,6 @@ package com.example.jonathan.inventoryassistant;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
